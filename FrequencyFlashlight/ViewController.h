9400f9b2611cf49fb4026d8b49990b4f8215f30f
frederike <freya.who@gmail.com>
2013-10-06 20:15:05 +0200

    first commit

--- FrequencyFlashlight/ViewController.h
+++ FrequencyFlashlight/ViewController.h
@@ -7,7 +7,42 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "FlashController.h"
 
 @interface ViewController : UIViewController
+{
+    NSTimer *strobeTimer;
+    NSTimer *strobeFlashTimer;
+    
+    IBOutlet UIButton *onButton;
+    IBOutlet UIButton *offButton;
+    
+    BOOL strobeIsOn;
+    BOOL strobeActivated; // To allow user to turn off the light all together
+	BOOL strobeFlashOn; // For our code to turn strobe on and off rapidly
+    
+    FlashController *flashController;
+}
+
+@property(nonatomic, retain) NSTimer *strobeTimer;
+@property (nonatomic, retain) NSTimer *strobeFlashTimer;
+@property(nonatomic, strong) IBOutlet IBOutlet UIButton *onButton;
+@property(nonatomic, strong) IBOutlet IBOutlet UIButton *offButton;
+
+@property (nonatomic, assign) BOOL strobeActivated;
+
+@property(nonatomic, retain) FlashController *flashController;
+
+-(IBAction)strobeOn:(id)sender; //combine powerbuttonpressed
+-(IBAction)strobeOff:(id)sender;
+
+- (IBAction)onStrobeSwitch:(id)sender;
+
+
+- (void)strobeTimerCallback:(id)sender;
+- (void)strobeFlashTimerCallback:(id)sender;
+
+- (void)startStopStrobe:(BOOL)strobeOn;
+
 
 @end
--- FrequencyFlashlight/ViewController.m
+++ FrequencyFlashlight/ViewController.m
@@ -14,11 +14,92 @@
 
 @implementation ViewController
 
+@synthesize strobeTimer, strobeFlashTimer;
+@synthesize offButton, onButton;
+@synthesize flashController;
+@synthesize strobeActivated;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    strobeIsOn = NO;
+	strobeActivated = NO;
+	strobeFlashOn = NO;
+	
+	flashController = [[FlashController alloc] init];
+	
+	self.strobeTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(strobeTimerCallback:) userInfo:nil repeats:YES];
+	self.strobeFlashTimer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(strobeFlashTimerCallback:) userInfo:nil repeats:YES];
+    
 }
+
+
+
+- (void)strobeTimerCallback:(id)sender {
+	if (strobeActivated) {
+		strobeIsOn = !strobeIsOn;
+		strobeFlashOn = YES;
+	} else {
+		strobeFlashOn = NO;
+	}
+}
+
+- (void)strobeFlashTimerCallback:(id)sender {
+	if (strobeFlashOn) {
+		strobeFlashOn = !strobeFlashOn;
+		[self startStopStrobe:strobeIsOn];
+	} else {
+		[self startStopStrobe:NO];
+	}
+}
+
+
+- (IBAction)strobeOn:(id)sender {
+	if(strobeActivated) {
+		strobeActivated = NO;
+	} else {
+		strobeActivated = YES;
+	}
+    
+	[self startStopStrobe:strobeActivated];
+}
+
+
+- (IBAction)strobeOff:(id)sender {
+	if(strobeActivated) {
+		strobeActivated = NO;
+	} else {
+		strobeActivated = YES;
+	}
+    
+	[self startStopStrobe:strobeActivated];
+}
+
+
+- (IBAction)onStrobeSwitch:(id)sender {
+	UISwitch *mySwitch = sender;
+	strobeActivated = mySwitch.on;
+	
+	[self startStopStrobe:strobeActivated];
+	
+	if(strobeActivated) {
+		
+	} else {
+		
+	}
+}
+
+- (void)startStopStrobe:(BOOL)strobeOnBool {
+	if (strobeOnBool) {
+		[flashController toggleStrobe:YES];
+	} else {
+		[flashController toggleStrobe:NO];
+	}
+}
+
 
 - (void)didReceiveMemoryWarning
 {
